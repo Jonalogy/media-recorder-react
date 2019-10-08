5525d6fb1446da89ff796c9c08981d7ba892a08a
jonalogy <jonalogy@gmail.com>
2019-10-08 09:10:26 +0800

    Updates

---
+++ precache-manifest.8f45d3d96d7ad843d9be8164b77b7989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67a0d54142b61b4742402e8ef2f3585",
+    "url": "/media-recorder-react/index.html"
+  },
+  {
+    "revision": "32ed78dac352689cc91a",
+    "url": "/media-recorder-react/static/css/main.47ce8cc1.chunk.css"
+  },
+  {
+    "revision": "cd9d1368ee836aba3908",
+    "url": "/media-recorder-react/static/js/2.a07915fb.chunk.js"
+  },
+  {
+    "revision": "32ed78dac352689cc91a",
+    "url": "/media-recorder-react/static/js/main.7fb9d5e5.chunk.js"
+  },
+  {
+    "revision": "de3b0a9453264608eeda",
+    "url": "/media-recorder-react/static/js/runtime-main.ef0963da.js"
+  }
+]);
