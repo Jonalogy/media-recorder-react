9d934662d378b40572002fbc72bd59a286c5ba7e
Jonathan Tan <jontan@palo-it.com>
2019-10-10 09:18:13 +0800

    Implemented react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import NativeRecorder from './recorders/Recorder';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Record Audio
-      </header>
-      <NativeRecorder />
+      <Router>
+        <header className="App-header">
+          Record Audio
+        </header>
+        
+        <Link to="/">Record</Link>
+        <br />
+        <Link to="/yay">Yay</Link>
+        
+        <Switch>
+          <Route exact path="/">
+            <NativeRecorder />
+          </Route>
+          <Route exact path="/yay">
+            <h1>Hey!</h1>
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
